8cc33a83da7ff3a7f02575674d99964bf861c84c
Ten-user <tentlaba13@gmail.com>
2025-11-14 11:20:36 +0200

    Update firebaseConfig.js

--- career-guidance-final-complete/client/src/firebaseConfig.js
+++ career-guidance-final-complete/client/src/firebaseConfig.js
@@ -1,1 +1,12 @@
-export default { apiKey:'', authDomain:'', projectId:'', storageBucket:'', messagingSenderId:'', appId:'' }
+// firebaseConfig.js
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_apiKey || '',
+  authDomain: import.meta.env.VITE_authDomain || '',
+  projectId: import.meta.env.VITE_projectId || '',
+  storageBucket: import.meta.env.VITE_storageBucket || '',
+  messagingSenderId: import.meta.env.VITE_messagingSenderId || '',
+  appId: import.meta.env.VITE_appId || '',
+  measurementId: import.meta.env.VITE_measurementId || ''
+};
+
+export default firebaseConfig;
